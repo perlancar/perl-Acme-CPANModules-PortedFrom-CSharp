6985080c04b956f2f9ae9f25ad0815901593e4fb
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-03-26 07:12:56 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/PortedFrom/CSharp.pm
@@ -0,0 +1,37 @@
+package Acme::CPANModules::PortedFrom::CSharp;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our $LIST = {
+    summary => "Modules/applications that are ported from (or inspired by) ".
+        "C#",
+    description => <<'_',
+
+If you know of others, please drop me a message.
+
+_
+    entries => [
+        {
+            module => 'LINQ',
+        },
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+
+=head1 prepend:SEE ALSO
+
+More on the same theme of modules ported from other languages:
+L<Acme::CPANModules::PortedFrom::Clojure>,
+L<Acme::CPANModules::PortedFrom::Java>,
+L<Acme::CPANModules::PortedFrom::NPM>,
+L<Acme::CPANModules::PortedFrom::PHP>,
+L<Acme::CPANModules::PortedFrom::Python>,
+L<Acme::CPANModules::PortedFrom::Ruby>.
